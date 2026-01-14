751a089963a037fcf06ff651acced9284ec782f1
Gillian Hanley <gghanley04@gmail.com>
2026-01-14 15:04:21 -0500

    change to title

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CGT390_Profile_App/',
+  base: '/CGT390_Project1/',
 })
 
